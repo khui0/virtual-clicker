989cd676e3a5351a32a6c71d1daf0e6aabc23b8f
Kenny <101839505+khui0@users.noreply.github.com>
2024-05-12 13:59:40 -0400

    feat: scale effect when clicking logo

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex flex-row items-center gap-3">
-  <a href="/"><Logo></Logo></a>
+  <a href="/" class="active:scale-95 transition-transform"><Logo></Logo></a>
   {#if $page.url.pathname === "/" || !$title}
     <h1 class="text-3xl font-bold">Virtual Clicker</h1>
   {:else}
