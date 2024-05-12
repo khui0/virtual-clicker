c2a827cc4b32caefa045523900069c4fd6e20243
Kenny <101839505+khui0@users.noreply.github.com>
2024-05-11 23:57:13 -0400

    feat: add history page

---
+++ src/lib/HistoryList.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { liveQuery } from "dexie";
+  import { db } from "./db";
+
+  let history = liveQuery(() => db.history.toArray());
+</script>
+
+<div role="list" class="flex flex-col gap-2 overflow-auto flex-1 mb-2 styled-scrollbar">
+  {#if $history}
+    {#each [...$history].reverse() as click}
+      <div class="flex flex-col border-input px-3 py-2 rounded-btn">
+        <div class="flex flex-row gap-2">
+          <div class="badge badge-neutral">{click.code}</div>
+          <h3>{click.question}</h3>
+        </div>
+        <p>
+          <span class="text-neutral-content"
+            >{click.date.toLocaleTimeString("en-US", {
+              timeStyle: "short",
+            })}</span
+          >
+          {click.response}
+        </p>
+      </div>
+    {/each}
+  {/if}
+</div>
---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { title } from "$lib/store";
+  title.set("History");
+
+  import HistoryList from "$lib/HistoryList.svelte";
+</script>
+
+<div class="h-full">
+  <HistoryList></HistoryList>
+</div>
