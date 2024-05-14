591c4a096e0a9e8fd1cff2e7a74f9b7efc082732
Kenny <101839505+khui0@users.noreply.github.com>
2024-05-14 18:33:32 -0400

    feat: close symbol picker on outside click

--- src/routes/SymbolsPicker.svelte
+++ src/routes/SymbolsPicker.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import SymbolsList from "./SymbolsList.svelte";
 
   import BiChevronDown from "~icons/bi/chevron-down";
+
+  let details: HTMLDetailsElement;
+
+  onMount(() => {
+    document.addEventListener("pointerup", (e) => {
+      if (!details) return;
+      const inside = details.contains(e.target as Node);
+      if (!inside) details.removeAttribute("open");
+    });
+  });
 </script>
 
-<details class="dropdown dropdown-end">
+<details class="dropdown dropdown-end" bind:this={details}>
   <summary class="btn btn-square focus"><BiChevronDown></BiChevronDown></summary>
   <div class="dropdown-content z-[1] shadow-lg bg-base-200 rounded-box w-52 mt-1 overflow-hidden">
     <ul class="menu max-h-80 p-2 overflow-auto flex-nowrap styled-scrollbar">
