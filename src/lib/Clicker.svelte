69126203d793241a5a525ed5e5529905f3de4d79
Kenny <101839505+khui0@users.noreply.github.com>
2024-05-13 20:59:30 -0400

    fix: tweak dropdown shadow

--- src/lib/Clicker.svelte
+++ src/lib/Clicker.svelte
@@ -175,7 +175,7 @@
       <details class="dropdown dropdown-end">
         <summary class="btn btn-square focus"><BiChevronDown></BiChevronDown></summary>
         <div
-          class="dropdown-content z-[1] shadow bg-base-200 rounded-box w-52 mt-1 overflow-hidden"
+          class="dropdown-content z-[1] shadow-lg bg-base-200 rounded-box w-52 mt-1 overflow-hidden"
         >
           <ul class="menu max-h-80 p-2 overflow-auto flex-nowrap styled-scrollbar">
             <SymbolsList
